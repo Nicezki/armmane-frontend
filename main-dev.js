213bf5bed6b2fb49b9b81bce41a151521c015dfb
Miri Shiwana <nicezki.jp@gmail.com>
2023-09-28 18:43:03 +0700

    fix some things

--- main-dev.js
+++ main-dev.js
@@ -750,8 +750,8 @@ class ARMMane{
 
     handlePrediction(data) {
         let prediction = JSON.parse(data);
-        this.changeText("prediction_class", "Class: " + prediction["current_result"] + " (" + prediction["confident_score"] + "%) <br> " + prediction["fps"] + " FPS <br> Detected:" + prediction["detect_flag"] + " ");
-        this.elements["ui"]["livepreview"].querySelector("img").src = "data:image/jpeg;base64," + prediction["current_frame"];
+        this.changeText("prediction_class", "Class: " + prediction["current_class"] + " (" + prediction["confident_score"] + "%) <br> " + prediction["fps"] + " FPS <br> Detected:" + prediction["detect_flag"] + " ");
+        this.elements["ui"]["livepreview"].querySelector("img").src = "data:image/jpeg;base64," + prediction["current_result"];
     }
 
 
